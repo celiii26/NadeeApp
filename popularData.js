08c0f01a6edae302ae7abe14fe87a681bac11d6c
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2023-05-02 18:36:59 +0700

    Add files via upload

---
+++ popularData.js
@@ -0,0 +1,28 @@
+const popularData = [
+  {
+    id: '1',
+    image: require('../images/azzahra.jpg'),
+    name: 'Az Zahra',
+    profesi: "Guru",
+    age: 27,
+  },
+  {
+    id: '2',
+    image: require('../images/putri.jpeg'),
+    name: 'Putri',
+    profesi: 'Dokter',
+    age: 25,
+
+  },
+  {
+    id: '3',
+    image: require('../images/annisa.jpg'),
+    name: 'Annisa',
+    profesi: "Guru",
+    age: 27,
+
+
+  },
+];
+
+export default popularData;
