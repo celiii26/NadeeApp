b3188b17af599ca07c33905ecd33d4c925f454ae
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2023-05-07 17:26:29 +0700

    new data

---
+++ assets/data/requestData.js
@@ -0,0 +1,37 @@
+const requestData = [
+    {
+      id: '1',
+      image: require('../images/azzahra.jpg'),
+      name: 'Fatimah Az Zahra',
+      profesi: "Guru",
+      age: 27,
+      region: "Medan, Sumatera Utara",
+      deskripsi: "Saya adalah orang yang penyabar dan sedang mencari pasangan hidup yang stabil mental dan finansial.",
+      domisili: "Medan"
+    },
+    {
+      id: '2',
+      image: require('../images/putri.jpeg'),
+      name: 'Zhafira Salsabila',
+      profesi: 'Dokter',
+      age: 25,
+      region: "Malang, Jawa Timur",
+      deskripsi: "Saya suka kucing dan mencari pasangan hidup yang bisa diajak merawat kucing.",
+      domisili: "Jakarta"
+    },
+    {
+      id: '3',
+      image: require('../images/annisa.jpg'),
+      name: 'Putri Khafifanisa',
+      profesi: "Guru",
+      age: 27,
+      region: "Surabaya, Jawa Timur",
+      deskripsi: "Saya adalah orang yang meyayangi orang tua dan anak anak.",
+      domisili: "Bandung"
+  
+  
+    },
+  ];
+  
+  export default requestData;
+  
