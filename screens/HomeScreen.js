b98bdaef1fbefb61846ea077a7928d0622a9e8b9
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2023-05-10 14:09:51 +0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,8 +6,6 @@ import { StyleSheet, Text, TouchableOpacity, View, Image,
   ScrollView,
   Button,
 } from 'react-native'
-import { auth } from '../firebase'
-import { signOut } from "firebase/auth";
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import popularData from '../assets/Data/popularData';
@@ -16,8 +14,6 @@ import PoppinsLight from '../assets/fonts/Poppins-Light.ttf'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
-  const user = auth.currentUser;
-  const email = user.email;
 
   const [isLoaded] = useFonts({
     PoppinsLight: PoppinsLight,
@@ -41,16 +37,6 @@ const HomeScreen = () => {
     
     <View style={styles.container}>
       <View style={styles.cardheader}>
-      <TouchableOpacity style={styles.detailButton} onPress={() => navigation.navigate('Profile')}>
-        <Text style={styles.buttonText}>Profile</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Request Inbox')}
-        style={styles.detailButton}
-      >
-        
-        <Text style={styles.buttonText}>Request Inbox</Text>
-      </TouchableOpacity>
       </View>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
@@ -75,16 +61,14 @@ const HomeScreen = () => {
             },
           ]}
         >
-          <View>
-            <View>
+          
               <View style={styles.popularTitlesWrapper}>
                 <Text style={styles.popularTitlesTitle}>{item.name}</Text>
                 <Text style={styles.popularTitlesTitle}>{item.age} tahun</Text>
                 <Text style={styles.popularTitlesTitle}>{item.profesi}</Text>
                 <Text style={styles.popularTitlesTitle}>{item.domisili}</Text>
               </View>
-            </View>
-          </View>
+          
 
           <View style={styles.popularCardRight}>
             <Image source={item.image} style={styles.popularCardImage} />
@@ -96,13 +80,6 @@ const HomeScreen = () => {
         </View>
         </ScrollView>
       
-
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.detailButton}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
     </View>
   )
 }
@@ -110,14 +87,6 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-  buttonProfile: {
-    backgroundColor: '#85586F',
-    width: '60%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-
-  },
   container: {
     flex: 1,
     backgroundColor: '#D8CBBB',
@@ -129,28 +98,6 @@ const styles = StyleSheet.create({
     flexDirection:'row' ,
     justifyContent: 'space-between',
    },
-  logoContainer: {
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logo: {
-    width: 98,
-    height: 94,
-  },
-   button: {
-    backgroundColor: '#0782F9',
-    width: '60%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginTop: 40,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '500',
-    fontSize: 14,
-  },
   headerWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -167,34 +114,11 @@ const styles = StyleSheet.create({
     marginTop: 30,
     paddingHorizontal: 20,
   },
-  titlesSubtitle: {
-    fontFamily: 'arial',
-    fontSize: 16,
-    color: 'black',
-  },
   titlesTitle: {
     fontFamily: 'arial',
     fontSize: 32,
     color: 'black',
     marginTop: 5,
-  },
-  searchWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    marginTop: 30,
-  },
-  search: {
-    flex: 1,
-    marginLeft: 10,
-    borderBottomColor: "white",
-    borderBottomWidth: 2,
-  },
-  searchText: {
-    fontFamily: 'arial',
-    fontSize: 14,
-    marginBottom: 5,
-    color: "black",
   },
   
   popularWrapper: {
@@ -227,11 +151,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  popularTopText: {
-    marginLeft: 10,
-    fontFamily: 'arial',
-    fontSize: 14,
-  },
   popularTitlesWrapper: {
     marginTop: 10,
     flexDirection: 'column',
@@ -242,53 +161,21 @@ const styles = StyleSheet.create({
     color: 'black',
     flexDirection: 'column',
   },
-  popularCardBottom: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 10,
-    marginLeft: 5,
-  },
-  detailButton: {
-    backgroundColor: "#85586F",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderTopRightRadius: 5,
-    borderTopLeftRadius: 5,
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
-    marginBottom: 5,
-    marginRight: 5,
-  },
-  buttonName: {
-    color: "#F5F5F5",
-    fontSize: 12,
-    fontFamily: 'arial',
-  },
-  ratingWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 20,
-  },
-  rating: {
-    fontFamily: 'arial',
-    fontSize: 12,
-    color: 'black',
-    marginLeft: 5,
-  },
+
   popularCardRight: {
-    position: 'abosulte',
-    marginRight:15,
- 
+    position: 'absolute', // Set the position property to 'absolute'
+    right: 5, // Align the image to the right of the card
+    top: 20, // Align the image to the top of the card
+    bottom: 0, // Align the image to the bottom of the card// Set the width of the image to half of the card's width
+    overflow: 'hidden',
   },
   popularCardImage: {
-    width: 150,
+    width: 90,
     height: 90,
     resizeMode: 'contain',
+    position: 'abosulte',
+    borderRadius: 75,
+    marginRight:15,
+
   },
-  asalDaerah: {
-    fontFamily: 'arial',
-    fontSize: 12,
-    color: 'black',
-    flexDirection: 'column',
-  }
 })
