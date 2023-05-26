02b61c4ab17583760b0128949c7536464deca494
Shely <88304373+celiii26@users.noreply.github.com>
2023-05-26 20:44:21 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,96 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import DetailScreen from './screens/DetailScreen';
-import RequestSent from './screens/RequestSent';
-import RequestInbox from './screens/RequestInbox';
-import EditProfileScreen from './screens/EditProfileScreen';
-import { useNavigation } from '@react-navigation/core';
-import ProgressTaaruf from './screens/ProgressTaaruf';
-import InboxDetailScreen from './screens/InboxDetailScreen';
-
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import DetailScreen from "./screens/DetailScreen";
+import RequestSent from "./screens/RequestSent";
+import RequestInbox from "./screens/RequestInbox";
+import EditProfileScreen from "./screens/EditProfileScreen";
+import { useNavigation } from "@react-navigation/core";
+import ProgressTaaruf from "./screens/ProgressTaaruf";
+import InboxDetailScreen from "./screens/InboxDetailScreen";
+import EdukasiTaaruf from "./screens/EdukasiTaaruf";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="HomeTab" component={Home} />
         <Stack.Screen
-          name="DetailScreen"
-          component={DetailScreen}
-          />
-        <Stack.Screen name="Request Sent" component={RequestSent} />
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="HomeTab"
+          component={Home}
+        />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Request Sent"
+          component={RequestSent}
+        />
         <Stack.Screen name="Progress" component={ProgressTaaruf} />
-        <Stack.Screen name="EditProfileScreen" component={EditProfileScreen} />
         <Stack.Screen name="InboxDetailScreen" component={InboxDetailScreen} />
+        <Stack.Screen name="EditProfileScreen" component={EditProfileScreen} />
+        <Stack.Screen name="EdukasiTaaruf" component={EdukasiTaaruf} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
-
 function Home() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
     <Tab.Navigator
-    initialRouteName="Home"
-    screenOptions={{
-      tabBarActiveTintColor: "#85586F",
-    }}
-  >
-    <Tab.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: "#85586F",
       }}
-    />
-    <Tab.Screen
-      name="Request Inbox"
-      component={RequestInbox}
-      options={{
-        tabBarLabel: 'Inbox',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="message" color={color} size={size} />
-        ),
-        tabBarBadge: 2,
-      }}
-    />
-    <Tab.Screen
-      name="Profile"
-      component={ProfileScreen}
-      options={{
-        tabBarLabel: 'Profile',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}
-    />
-     </Tab.Navigator>
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Request Inbox"
+        component={RequestInbox}
+        options={{
+          tabBarLabel: "Inbox",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="message" color={color} size={size} />
+          ),
+          tabBarBadge: 2,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
